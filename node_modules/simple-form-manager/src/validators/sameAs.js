5ebd41fb896e964e0b298f9f2dffde6079d7b9ef
Bill Haack <bhaack360@gmail.com>
2020-10-17 13:22:37 -0700

    save

--- node_modules/simple-form-manager/src/validators/sameAs.js
+++
@@ -1,5 +0,0 @@
-import { ref, withParams } from "./common";
-export default (equalTo) =>
-	withParams({ type: "sameAs", eq: equalTo }, (value, parentVm) => {
-		return value === ref(equalTo, this, parentVm);
-	});
